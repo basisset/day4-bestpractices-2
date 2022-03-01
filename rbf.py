a9585ea7911acd8719dc3e3725af9064ff545437
basisset <pamelasvensson@gmail.com>
2022-03-01 17:40:34 +0100

    Speed optimization using Cython

--- rbf.py
+++ rbf.py
@@ -3,6 +3,7 @@ import numpy as np
 from scipy.interpolate import Rbf
 
 # Naive python implementation of a Radial Basis Function (RBF) approximation scheme
+#@profile ### time = 10s
 def rbf_network(X, beta, theta):
 
     N = X.shape[0]
@@ -12,19 +13,20 @@ def rbf_network(X, beta, theta):
     for i in range(N):
         for j in range(N):
             r = 0
-            for d in range(D):
-                r += (X[j, d] - X[i, d]) ** 2
+            for d in range(D):               ### Loop in a loop slows things down
+                r += (X[j, d] - X[i, d]) ** 2### Elementwise subtraction takes a long time 
             r = r**0.5
             Y[i] += beta[j] * exp(-(r * theta)**2)
 
     return Y
 
 # Scipy implementation of a Radial Basis Function (RBF) approximation scheme
+#@profile ### time = 0.053 s, 200 times faster
 def rbf_scipy(X, beta):
 
     N = X.shape[0]
     D = X.shape[1]    
-    rbf = Rbf(X[:,0], X[:,1], X[:,2], X[:,3], X[:, 4], beta)
+    rbf = Rbf(X[:,0], X[:,1], X[:,2], X[:,3], X[:, 4], beta) ### Scipy already has this implemented
     #Xtuple = tuple([X[:, i] for i in range(D)])
     Xtuple = tuple([X[:, i] for i in range(D)])
 
@@ -35,7 +37,7 @@ def rbf_scipy(X, beta):
 # 
 # TODO: Write the Cython implementation in a separate fastloop.pyx file, compile and import it here
 # 
-# from fastloop import rbf_network_cython
+from fastloop import rbf_network_cython
 
 
 # Make up some data
@@ -46,7 +48,13 @@ beta = np.random.rand(N)
 theta = 10
 
 # Simple testing of the performance of the Python and Scipy implementations
+#@profile
 import time
+
+
+#rbf_network(X, beta, theta)
+
+#rbf_scipy(X, beta)
 
 t0 = time.time()
 rbf_network(X, beta, theta)
@@ -57,9 +65,10 @@ rbf_scipy(X, beta)
 print("Scipy: ", time.time() - t0)
 
 # Testing the performance of Cython
-#t0 = time.time()
-#rbf_network_cython(X, beta, theta)
-#print("Cython: ", time.time() - t0)
+### time = 0.055 s, fasten than Python bit still slower than cython
+t0 = time.time()
+rbf_network_cython(X, beta, theta)
+print("Cython: ", time.time() - t0)
 
 
 
